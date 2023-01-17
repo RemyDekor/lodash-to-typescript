e2204094d13cdc024a985a202f22ff7b2ad506b5
Rémy Bonté-Duval <remy.bonteduval@prosehair.com>
2023-01-17 16:54:32 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ const logLibrariesResults = ([loRes, reResJs, reResTs, rbRes]: [unknown, unknown
   console.log("rambda", rbRes);
 }
 
+
 const isUnexpectedValue = Math.random() > 0.5
 
 const createObj = () => ({
@@ -19,24 +20,35 @@ const createObj = () => ({
   arrNumber: [1, 2, 3, 4],
   arrNullable: isUnexpectedValue ? null : [1, 2, 3, 4],
   arrRand: isUnexpectedValue ? ['a', 'b', 'c'] : [1, 2, 3, 4],
+  nested: { foo: { bar: isUnexpectedValue ? null : { thingy: [42] } } }
 });
 
-const obj = createObj(); // use this "null" to see how different libs handle it (hint: zod might help us with libs other than lodash)
+const obj = createObj(); // null // use this "null" to see how different libs handle it (hint: zod might help us with libs other than lodash)
 console.table(obj)
 
 const loObj = _.clone(obj);
 const reObj = Re.clone(obj);
 const rbObj = Rb.clone(obj);
 
-console.log('--------------- get ---------------');
-console.log("vanilla 'get': ", obj?.strNullable);
+console.log('--------------- get a nullable ---------------');
 
-const loRes = _.get('strNullable')(loObj);
-const reResJs = Re.pathOr(['strNullable'], undefined)(reObj); // 🟠 does not use 'null', and uses fallback value instead
-const reResTs = Re.pathOr(reObj, ['strNullable'], undefined); // 🟠 does not use 'null', and uses fallback value instead
+const vanillaRes = obj?.strNullable; // ✅ knows the type, as expected
+console.log("vanilla 'get': ", vanillaRes);
+const loRes = _.get('strNullable')(loObj); // 🟠 does not know what type we are getting
+const reResJs = Re.pathOr(['strNullable'], undefined)(reObj); // 🟠 does not use 'null', and uses fallback value instead 🟠 not usable in TS 
+const reResTs = Re.pathOr(reObj, ['strNullable'], undefined); // 🟠 does not use 'null', and uses fallback value instead, stops screaming if fallback is an actual value (e.g. 'abc')
 const rbRes = Rb.path(['strNullable'])(rbObj); // 🟠 null becomes undefined
 
 logLibrariesResults([loRes, reResJs, reResTs, rbRes])
+
+console.log('--------------- set ---------------');
+
+const loSetRes = _.set('strNullable', 'an other thing')(loObj); // ✅ keeps the output's type
+const reSetResJs = Re.set('strNullable', 'an other thing')(reObj); // 🟠 works fine in JS, but TS is yelling at us + 🟠 loses the output's type
+const reSetResTs = Re.set(reObj, 'strNullable', 'an other thing'); // ✅ keeps the output's type
+const rbSetRes = Rb.set(Rb.lensProp('strNullable'), 'an other thing')(rbObj); // 🟠 awkward lens syntax, ✅ but keeps the output's type
+
+logLibrariesResults([loSetRes, reSetResJs, reSetResTs, rbSetRes])
 
 console.log('--------------- filter array ---------------');
 
@@ -52,39 +64,27 @@ console.log('--------------- filter a nullable array ---------------');
 const isEven = (n: number): boolean => n % 2 === 0
 
 const loFilterNullableRes = _.filter(isEven)(_.get('arrNullable', loObj));  // ✅ 👻 Accepts `null` (outputs `[]`)
-const reFilterNullableResJs = Re.createPipe(Re.filter(isEven))(reObj?.arrNullable || []); // 🟠 Does not accept nullable, warns with Typescript
+const reFilterNullableResJs = Re.filter(isEven)(reObj?.arrNullable || []); // 🟠 Does not accept nullable, warns with Typescript
 const reFilterNullableResTs = Re.filter(reObj?.arrNullable || [], isEven); // 🟠 Does not accept nullable, warns with Typescript
 const rbFilterNullableRes = Rb.filter(isEven)(rbObj?.arrNullable || []); // 🟠 Does not accept nullable, warns with Typescript
 
 logLibrariesResults([loFilterNullableRes, reFilterNullableResJs, reFilterNullableResTs, rbFilterNullableRes]);
-// console.log('--------------- flow/pipe ---------------');
 
-// const loPipeRes = _.flow(_.get('arrNumber'), _.filter(n => (n % 2 === 0)), _.map(_.toString))(loObj); // gets output type correctly only because _.toString
-// const rePipeResJs = Re.createPipe(Re.filter(n => (n % 2 === 0)), Re.map(n => n.toString()))(reObj?.arrNumber);
-// const rePipeResTs = Re.pipe(reObj?.arrNumber, Re.filter(n => (n % 2 === 0)), Re.map(n => n.toString())); // gets n's type correctly + output type without a specific toString() function
-// const rbPipeResTs = Rb.pipe(Rb.filter(n => n % 2 === 0), Rb.map(Rb.toString))(rbObj?.arrNumber); // gets output type correctly only because Rb.toString
+console.log('--------------- flow/pipe ---------------');
 
-// console.log('---');
-// console.log("lodash/fp", loPipeRes);
-// console.log("remeda data-last", rePipeResJs);
-// console.log("remeda data-first", rePipeResTs);
-// console.log("rambda", rbPipeResTs);
+const isNumber = (x: any): x is number => typeof x === 'number';
 
-// const isNumber = (x: any): x is number => typeof x === 'number';
+const loRandPipeRes = _.flow(_.get('arrRand'), _.filter(_.isNumber), _.filter(n => (n % 2 === 0)))(loObj);
+const reRandPipeResJs = Re.createPipe(Re.filter(Re.isNumber), Re.filter(n => (n % 2 === 0)), )(reObj?.arrRand);
+const reRandPipeResTs1 = Re.pipe(reObj?.arrRand, Re.filter(Re.isNumber), Re.filter(n => (n % 2 === 0))); // not sure why TS is unhappy (something about output == never[])
+const reRandPipeResTs2 = Re.pipe(reObj?.arrRand, Re.filter(isNumber), Re.filter(n => (n % 2 === 0))); // not sure why using custom isNumber instead of Re.isNumber fixes it
+const rbRandPipeRes = Rb.pipe(Rb.filter(isNumber), Rb.filter(n => (n % 2 === 0)))(rbObj?.arrRand) // Rambda does not have isNumber
 
-// const loRandPipeRes = _.flow(_.get('arrRand'), _.filter(_.isNumber), _.filter(n => (n % 2 === 0)))(loObj);
-// const reRandPipeResJs = Re.createPipe(Re.filter(Re.isNumber), Re.filter(n => (n % 2 === 0)), )(reObj?.arrRand);
-// const reRandPipeResTs1 = Re.pipe(reObj?.arrRand, Re.filter(Re.isNumber), Re.filter(n => (n % 2 === 0)));
-// const reIsNumber = (x: any): x is number => Re.isNumber(x) // needed this instead of Re.isNumber - maybe bc of nullable (see reRandPipeResTs1 vs reRandPipeResTs2)
-// const reRandPipeResTs2 = Re.pipe(reObj?.arrRand, Re.filter(reIsNumber), Re.filter(n => (n % 2 === 0)));
-// const rbRandPipeRes = Rb.pipe(Rb.filter(isNumber), Rb.filter(n => (n % 2 === 0)))(rbObj?.arrRand) // does not have Rambda.isNumber
-
-// console.log('---');
-// console.log("lodash/fp", loRandPipeRes);
-// console.log("remeda data-last", reRandPipeResJs);
-// console.log("remeda data-first 1", reRandPipeResTs1);
-// console.log("remeda data-first 2", reRandPipeResTs2);
-// console.log("rambda", rbRandPipeRes);
+console.log("lodash/fp", loRandPipeRes);
+console.log("remeda data-last", reRandPipeResJs);
+console.log("remeda data-first 1", reRandPipeResTs1);
+console.log("remeda data-first 2", reRandPipeResTs2);
+console.log("rambda", rbRandPipeRes);
 
 // things to consider:
 // - behaviours around 'null' and undefined values
@@ -97,9 +97,10 @@ logLibrariesResults([loFilterNullableRes, reFilterNullableResJs, reFilterNullabl
 
 // Remeda:
 // - Doc says it infers types from the data => but only if data-first
-// - Somewhat support Typescript when data-first, but TBC (I got confused a few times, see reRandPipeResTs1/reRandPipeResTs2)
-// - Not Typescript-friendly when data-last
+// - Supports Typescript when data-first (a few edge-cases can be confusing, but it seems like it does)
+// - Typescript yells at us when data-last (almost always)
 // - NOT null-friendly (e.g. pathOr considers 'null' as not being a value, and uses the default instead (lodash keeps 'null' in this case))
+
 
 // Rambda
 // seems very similar to lodash/fp (except for the null-friendly part)
