5eab94ea46439caba377a897a2239b99d36ba154
Rémy Bonté-Duval <remy.bonteduval@prosehair.com>
2023-01-17 11:20:27 +0100

    wip filter a nullable array

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css'
 
-import _, { LodashRest } from 'lodash/fp';
+import _ from 'lodash/fp';
 import * as Re from 'remeda';
 import * as Rb from 'rambda'; // minimal alternative of ramda (more complete version => rambdax)
 // import * as R from 'ramda';
@@ -15,9 +15,10 @@ const logLibrariesResults = ([loRes, reResJs, reResTs, rbRes]: [unknown, unknown
 const isUnexpectedValue = Math.random() > 0.5
 
 const createObj = () => ({
-  strRand: isUnexpectedValue ? null : 'something',
-  arrNumber: [101, 102, 103, 104],
-  arrRand: isUnexpectedValue ? ['a', 'b', 'c'] : [1, 2, 3],
+  strNullable: isUnexpectedValue ? null : 'something',
+  arrNumber: [1, 2, 3, 4],
+  arrNullable: isUnexpectedValue ? null : [1, 2, 3, 4],
+  arrRand: isUnexpectedValue ? ['a', 'b', 'c'] : [1, 2, 3, 4],
 });
 
 const obj = createObj(); // null; // use this "null" to see how different libs handle it (hint: zod might help us with libs other than lodash)
@@ -29,57 +30,63 @@ const reObj = Re.clone(obj);
 const rbObj = Rb.clone(obj);
 
 console.log('--------------- get ---------------');
-console.log("vanilla 'get': ", obj?.strRand);
+console.log("vanilla 'get': ", obj?.strNullable);
 
-const loRes = _.get('strRand')(loObj);
+const loRes = _.get('strNullable')(loObj);
 // remeda Data-last (js)
-const reResJs = Re.pathOr(['strRand'], undefined)(reObj); // does not use 'null', and uses fallback value instead
+const reResJs = Re.pathOr(['strNullable'], undefined)(reObj); // does not use 'null', and uses fallback value instead
 // remeda Data-first (ts)
-const reResTs = Re.pathOr(reObj, ['strRand'], undefined); // does not use 'null', and uses fallback value instead
-const rbRes = Rb.path(['strRand'])(rbObj); // null becomes undefined
+const reResTs = Re.pathOr(reObj, ['strNullable'], undefined); // does not use 'null', and uses fallback value instead
+const rbRes = Rb.path(['strNullable'])(rbObj); // null becomes undefined
 
 logLibrariesResults([loRes, reResJs, reResTs, rbRes])
 
-console.log('--------------- filter ---------------');
+console.log('--------------- filter array ---------------');
 
-// const loFilterRes = _.filter(n => (n % 2 === 0))(loObj); // gets output type correctly only because _.toString
-// const reFilterResJs = Re.createPipe(Re.filter(n => (n % 2 === 0)), Re.map(n => n.toString()))(reObj?.arrNumber);
-// const reFilterResTs = Re.pipe(reObj?.arrNumber, Re.filter(n => (n % 2 === 0)), Re.map(n => n.toString())); // gets n's type correctly + output type without a specific toString() function
-// const rbFilterResTs = Rb.pipe(Rb.filter(n => n % 2 === 0), Rb.map(Rb.toString))(rbObj?.arrNumber); // gets output type correctly only because Rb.toString
+const loFilterRes = _.filter(n => (n % 2 === 0))(_.get('arrNumber', loObj));  // ❌ can NOT infer n's type ❌ neither output type (unknown[])
+const reFilterResJs = Re.filter(n => (n % 2 === 0))(reObj.arrNumber); // ❌ can NOT infer n's type ❌ neither output type (unknown[])
+const reFilterResTs = Re.filter(reObj.arrNumber, n => (n % 2 === 0)); // ✅ can infer n's type + ✅ output type (number[])
+const rbFilterRes = Rb.filter(n => n % 2 === 0)(rbObj.arrNumber); // ❌ can NOT infer n's type ❌ neither output type (unknown[])
 
-// console.log("lodash/fp", loFilterRes);
-// console.log("remeda data-last", reFilterResJs);
-// console.log("remeda data-first", reFilterResTs);
-// console.log("rambda", rbFilterResTs);
+logLibrariesResults([loFilterRes, reFilterResJs, reFilterResTs, rbFilterRes]);
 
-console.log('--------------- flow/pipe ---------------');
+console.log('--------------- filter a nullable array ---------------');
 
-const loPipeRes = _.flow(_.get('arrNumber'), _.filter(n => (n % 2 === 0)), _.map(_.toString))(loObj); // gets output type correctly only because _.toString
-const rePipeResJs = Re.createPipe(Re.filter(n => (n % 2 === 0)), Re.map(n => n.toString()))(reObj?.arrNumber);
-const rePipeResTs = Re.pipe(reObj?.arrNumber, Re.filter(n => (n % 2 === 0)), Re.map(n => n.toString())); // gets n's type correctly + output type without a specific toString() function
-const rbPipeResTs = Rb.pipe(Rb.filter(n => n % 2 === 0), Rb.map(Rb.toString))(rbObj?.arrNumber); // gets output type correctly only because Rb.toString
+const loFilterNullableRes = _.filter(n => (n % 2 === 0))(_.get('arrNullable', loObj));  // 👻 Accepts `null` (outputs `[]` with unknown[] type)
+const reFilterNullableResJs = Re.filter(n => (n % 2 === 0))(reObj?.arrNullable); // 🛑 Does not accept nullable, but warns with Typescript
+const reFilterNullableResTs = Re.filter(reObj?.arrNullable, n => (n % 2 === 0)); // 🛑 Does not accept nullable, but warns with Typescript
+const rbFilterNullableRes = Rb.filter(n => n % 2 === 0)(rbObj?.arrNullable); // 🛑 Does not accept nullable, but warns with Typescript
 
-console.log('---');
-console.log("lodash/fp", loPipeRes);
-console.log("remeda data-last", rePipeResJs);
-console.log("remeda data-first", rePipeResTs);
-console.log("rambda", rbPipeResTs);
+logLibrariesResults([loFilterNullableRes, reFilterNullableResJs, reFilterNullableResTs, rbFilterNullableRes]);
 
-const isNumber = (x: any): x is number => typeof x === 'number';
+// console.log('--------------- flow/pipe ---------------');
 
-const loRandPipeRes = _.flow(_.get('arrRand'), _.filter(_.isNumber), _.filter(n => (n % 2 === 0)))(loObj);
-const reRandPipeResJs = Re.createPipe(Re.filter(Re.isNumber), Re.filter(n => (n % 2 === 0)), )(reObj?.arrRand);
-const reRandPipeResTs1 = Re.pipe(reObj?.arrRand, Re.filter(Re.isNumber), Re.filter(n => (n % 2 === 0)));
-const reIsNumber = (x: any): x is number => Re.isNumber(x) // needed this instead of Re.isNumber - maybe bc of nullable (see reRandPipeResTs1 vs reRandPipeResTs2)
-const reRandPipeResTs2 = Re.pipe(reObj?.arrRand, Re.filter(reIsNumber), Re.filter(n => (n % 2 === 0)));
-const rbRandPipeRes = Rb.pipe(Rb.filter(isNumber), Rb.filter(n => (n % 2 === 0)))(rbObj?.arrRand) // does not have Rambda.isNumber
+// const loPipeRes = _.flow(_.get('arrNumber'), _.filter(n => (n % 2 === 0)), _.map(_.toString))(loObj); // gets output type correctly only because _.toString
+// const rePipeResJs = Re.createPipe(Re.filter(n => (n % 2 === 0)), Re.map(n => n.toString()))(reObj?.arrNumber);
+// const rePipeResTs = Re.pipe(reObj?.arrNumber, Re.filter(n => (n % 2 === 0)), Re.map(n => n.toString())); // gets n's type correctly + output type without a specific toString() function
+// const rbPipeResTs = Rb.pipe(Rb.filter(n => n % 2 === 0), Rb.map(Rb.toString))(rbObj?.arrNumber); // gets output type correctly only because Rb.toString
 
-console.log('---');
-console.log("lodash/fp", loRandPipeRes);
-console.log("remeda data-last", reRandPipeResJs);
-console.log("remeda data-first 1", reRandPipeResTs1);
-console.log("remeda data-first 2", reRandPipeResTs2);
-console.log("rambda", rbRandPipeRes);
+// console.log('---');
+// console.log("lodash/fp", loPipeRes);
+// console.log("remeda data-last", rePipeResJs);
+// console.log("remeda data-first", rePipeResTs);
+// console.log("rambda", rbPipeResTs);
+
+// const isNumber = (x: any): x is number => typeof x === 'number';
+
+// const loRandPipeRes = _.flow(_.get('arrRand'), _.filter(_.isNumber), _.filter(n => (n % 2 === 0)))(loObj);
+// const reRandPipeResJs = Re.createPipe(Re.filter(Re.isNumber), Re.filter(n => (n % 2 === 0)), )(reObj?.arrRand);
+// const reRandPipeResTs1 = Re.pipe(reObj?.arrRand, Re.filter(Re.isNumber), Re.filter(n => (n % 2 === 0)));
+// const reIsNumber = (x: any): x is number => Re.isNumber(x) // needed this instead of Re.isNumber - maybe bc of nullable (see reRandPipeResTs1 vs reRandPipeResTs2)
+// const reRandPipeResTs2 = Re.pipe(reObj?.arrRand, Re.filter(reIsNumber), Re.filter(n => (n % 2 === 0)));
+// const rbRandPipeRes = Rb.pipe(Rb.filter(isNumber), Rb.filter(n => (n % 2 === 0)))(rbObj?.arrRand) // does not have Rambda.isNumber
+
+// console.log('---');
+// console.log("lodash/fp", loRandPipeRes);
+// console.log("remeda data-last", reRandPipeResJs);
+// console.log("remeda data-first 1", reRandPipeResTs1);
+// console.log("remeda data-first 2", reRandPipeResTs2);
+// console.log("rambda", rbRandPipeRes);
 
 // things to consider:
 // - behaviours around 'null' and undefined values
